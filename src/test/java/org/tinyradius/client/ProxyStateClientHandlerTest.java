931d6f0e731554b160858c92c69f377789569190
Mark Agyapong <mark.agyapong@globalreachtech.com>
2019-07-17 10:55:02 +0100

    proxy state client handler create attribute test

---
+++ src/test/java/org/tinyradius/client/ProxyStateClientHandlerTest.java
@@ -0,0 +1,40 @@
+package org.tinyradius.client;
+
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.util.HashedWheelTimer;
+import org.junit.jupiter.api.Test;
+import org.tinyradius.attribute.RadiusAttribute;
+import org.tinyradius.dictionary.DefaultDictionary;
+import org.tinyradius.packet.AccessRequest;
+import org.tinyradius.packet.RadiusPacket;
+import org.tinyradius.util.RadiusEndpoint;
+
+import java.net.InetSocketAddress;
+import java.security.SecureRandom;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ProxyStateClientHandlerTest {
+
+    private static DefaultDictionary dictionary = DefaultDictionary.INSTANCE;
+
+    @Test()
+    void processRequestCreateProxyStateAttribute() {
+        int id = new SecureRandom().nextInt(256);
+        HashedWheelTimer timer = new HashedWheelTimer();
+        NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup(4);
+        ProxyStateClientHandler handler = new ProxyStateClientHandler(dictionary, timer, 1000, address -> "secret");
+
+        final RadiusPacket packet = new AccessRequest(dictionary, id, null).encodeRequest("test");
+        final RadiusEndpoint endpoint = new RadiusEndpoint(new InetSocketAddress(0), "test");
+
+        handler.processRequest(packet, endpoint, eventLoopGroup.next());
+
+        List<RadiusAttribute> attributes = packet.getAttributes();
+        assertEquals(1, attributes.size());
+        assertNotNull(packet.getAttribute("Proxy-State"));
+
+    }
+}
