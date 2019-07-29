6181153a9b636600353a7737bfa4997e0c77d1e7
Horace Li <horace.li@globalreachtech.com>
2019-07-29 19:44:40 +0100

    stub test

---
+++ src/test/java/org/tinyradius/server/HandlerAdapterTest.java
@@ -0,0 +1,14 @@
+package org.tinyradius.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HandlerAdapterTest {
+
+    @Test
+    void channelRead0() {
+
+        // todo
+    }
+}
