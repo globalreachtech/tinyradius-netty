86f409fb81a44b291359df6b75ff813269f69ee5
Horace Li <horace.li@globalreachtech.com>
2020-01-02 13:04:56 +0000

    fix tests

--- src/test/java/org/tinyradius/server/handler/RequestHandlerTest.java
+++ src/test/java/org/tinyradius/server/handler/RequestHandlerTest.java
@@ -3,29 +3,42 @@ package org.tinyradius.server.handler;
 import io.netty.channel.ChannelHandlerContext;
 import org.junit.jupiter.api.Test;
 import org.tinyradius.dictionary.DefaultDictionary;
-import org.tinyradius.dictionary.Dictionary;
 import org.tinyradius.packet.AccessRequest;
+import org.tinyradius.packet.AccountingRequest;
 import org.tinyradius.packet.RadiusPacket;
 import org.tinyradius.server.RequestCtx;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RequestHandlerTest {
 
-    private final Dictionary dictionary = DefaultDictionary.INSTANCE;
-
-    private final RequestHandler handler = new RequestHandler() {
-        @Override
-        protected void channelRead0(ChannelHandlerContext ctx, RequestCtx msg) {
-
-        }
-    };
+    private final RadiusPacket accessRequest = new AccessRequest(DefaultDictionary.INSTANCE, 1, null);
 
     @Test
-    void acceptInboundMessage() throws Exception {
-        final RadiusPacket packet = new AccessRequest(dictionary, 1, null);
+    void acceptMsg() throws Exception {
 
-        final boolean b = handler.acceptInboundMessage(new RequestCtx(packet, null));
+        final boolean b = new RequestHandler() {
+            @Override
+            protected void channelRead0(ChannelHandlerContext ctx, RequestCtx msg) {
+            }
+        }.acceptInboundMessage(new RequestCtx(accessRequest, null));
+        assertTrue(b);
+    }
+
+    @Test
+    void rejectMsg() throws Exception {
+
+        final boolean b = new RequestHandler() {
+            @Override
+            protected Class<? extends RadiusPacket> acceptedPacketType() {
+                return AccountingRequest.class;
+            }
+
+            @Override
+            protected void channelRead0(ChannelHandlerContext ctx, RequestCtx msg) {
+            }
+        }.acceptInboundMessage(new RequestCtx(accessRequest, null));
         assertFalse(b);
     }
 }
