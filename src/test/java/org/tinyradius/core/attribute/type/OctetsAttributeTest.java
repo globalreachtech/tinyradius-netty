7d9c6bb2cf915459f1d5adca331f510d3cd06430
Horace Li <horaceli@gmail.com>
2020-07-08 17:45:57 +0100

    OctetsAttributeTest encoding tests

--- src/test/java/org/tinyradius/core/attribute/type/OctetsAttributeTest.java
+++ src/test/java/org/tinyradius/core/attribute/type/OctetsAttributeTest.java
@@ -1,15 +1,18 @@
 package org.tinyradius.core.attribute.type;
 
 import org.junit.jupiter.api.Test;
+import org.tinyradius.core.RadiusPacketException;
+import org.tinyradius.core.attribute.type.decorator.EncodedAttribute;
 import org.tinyradius.core.dictionary.DefaultDictionary;
 import org.tinyradius.core.dictionary.Dictionary;
 
 import java.security.SecureRandom;
 
 import static java.lang.Byte.toUnsignedInt;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.*;
 
-class RadiusAttributeTest {
+class OctetsAttributeTest {
 
     private static final SecureRandom random = new SecureRandom();
     private static final Dictionary dictionary = DefaultDictionary.INSTANCE;
@@ -42,5 +45,38 @@ class RadiusAttributeTest {
     void testToString() {
         final RadiusAttribute attribute = new OctetsAttribute(dictionary, -1, (byte) 2, "123456");
         assertEquals("User-Password: 123456", attribute.toString());
+    }
+
+    @Test
+    void encodeDecode() throws RadiusPacketException {
+        final String secret = "mySecret";
+        final String pw = "myPw";
+
+        final byte[] requestAuth = random.generateSeed(16);
+
+        final RadiusAttribute attribute = dictionary.createAttribute(-1, (byte) 2, (byte) 0, pw);
+        assertTrue(attribute instanceof OctetsAttribute);
+        assertFalse(attribute.isEncoded());
+        assertEquals(pw, new String(attribute.getValue(), UTF_8));
+
+        // encode
+        final RadiusAttribute encode = attribute.encode(requestAuth, secret);
+        assertTrue(encode instanceof EncodedAttribute);
+        assertTrue(encode.isEncoded());
+        assertNotEquals(pw, new String(encode.getValue(), UTF_8));
+
+        // encode again
+        final RadiusAttribute encode1 = encode.encode(requestAuth, secret);
+        assertEquals(encode1, encode);
+
+        // decode
+        final RadiusAttribute decode = encode.decode(requestAuth, secret);
+        assertTrue(decode instanceof OctetsAttribute);
+        assertFalse(decode.isEncoded());
+        assertEquals(pw, new String(decode.getValue(), UTF_8));
+
+        // decode again
+        final RadiusAttribute decode1 = decode.decode(requestAuth, secret);
+        assertEquals(decode1, decode);
     }
 }
