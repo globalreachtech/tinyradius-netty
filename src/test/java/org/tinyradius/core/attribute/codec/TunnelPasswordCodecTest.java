462a8aa17af9778aadceae70dc44a2ff4da1ae8f
Horace Li <horaceli@gmail.com>
2020-07-08 17:45:33 +0100

    tests for codec encoding numbers

--- src/test/java/org/tinyradius/core/attribute/codec/TunnelPasswordCodecTest.java
+++ src/test/java/org/tinyradius/core/attribute/codec/TunnelPasswordCodecTest.java
@@ -1,5 +1,6 @@
 package org.tinyradius.core.attribute.codec;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.tinyradius.core.attribute.type.RadiusAttribute;
@@ -7,7 +8,9 @@ import org.tinyradius.core.dictionary.DefaultDictionary;
 import org.tinyradius.core.dictionary.Dictionary;
 import org.tinyradius.core.RadiusPacketException;
 
+import java.nio.ByteBuffer;
 import java.security.SecureRandom;
+import java.util.Arrays;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.*;
@@ -35,8 +38,22 @@ class TunnelPasswordCodecTest {
 
         final byte[] decode = codec.decode(encode, requestAuth, sharedSecret);
         assertEquals(password, new String(decode, UTF_8));
+    }
 
-        final RadiusAttribute attribute2 = dictionary.createAttribute(-1, TUNNEL_PASSWORD, password.getBytes(UTF_8));
-        assertEquals(password, new String(attribute2.getValue(), UTF_8));
+    @Test
+    void encodeDecodeNumber() throws RadiusPacketException {
+        final int pw = 12345;
+        final byte[] pwByte = ByteBuffer.allocate(4).putInt(pw).array();
+        final byte[] requestAuth = random.generateSeed(16);
+        final String sharedSecret = "sharedSecret1";
+
+        final RadiusAttribute attribute = dictionary.createAttribute(-1, (byte) 141, pwByte);
+        assertArrayEquals(pwByte, attribute.getValue());
+
+        final byte[] encode = codec.encode(attribute.getValue(), requestAuth, sharedSecret);
+        assertNotEquals(Arrays.toString(pwByte), Arrays.toString(encode));
+
+        final byte[] decode = codec.decode(encode, requestAuth, sharedSecret);
+        assertArrayEquals(pwByte, decode);
     }
 }
