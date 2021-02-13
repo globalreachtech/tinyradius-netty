96d64c0acdb284ef9e456f04085f99bf0e586e5d
Lee Painter <lee@ludup.com>
2021-02-13 17:03:06 +0000

    Fixed issues requested in pull request review.

--- src/main/java/org/tinyradius/io/server/SecretProvider.java
+++ src/main/java/org/tinyradius/io/server/SecretProvider.java
@@ -6,25 +6,28 @@ import org.tinyradius.core.packet.request.RadiusRequest;
 
 public interface SecretProvider {
 
-    /**
-     * Returns the shared secret used to communicate with the client/host with the
-     * passed IP address or null if the client is not allowed at this server.
-     *
-     * @param address IP address and port number of remote host/client
-     * @return shared secret or null
-     */
-    String getSharedSecret(InetSocketAddress address);
-    
-    /**
-     * An alternative method of returning shared secret but this time with the current
-     * radius request so that alternative secrets can be returned based on some sort of 
-     * context. 
-     * 
-     * By default this method calls the original getSharedSecret.
-     * @param address
-     * @param request
-     * @return
-     */
-    default String getSharedSecret(InetSocketAddress address, RadiusRequest request) { return getSharedSecret(address); }
+	/**
+	 * Returns the shared secret used to communicate with the client/host with the
+	 * passed IP address or null if the client is not allowed at this server.
+	 *
+	 * @param address IP address and port number of remote host/client
+	 * @return shared secret or null
+	 */
+	String getSharedSecret(InetSocketAddress address);
+
+	/**
+	 * An alternative method of returning shared secret but this time with the
+	 * current radius request so that alternative secrets can be returned based on
+	 * some sort of context.
+	 * 
+	 * By default this method calls the original getSharedSecret.
+	 * 
+	 * @param address IP address and port number of remote host/client
+	 * @param request the RadiusRequest relating to this request
+	 * @return shared secret or null
+	 */
+	default String getSharedSecret(InetSocketAddress address, RadiusRequest request) {
+		return getSharedSecret(address);
+	}
 
 }
